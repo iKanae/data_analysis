76f6da8f9403c0f4d2d2c4274c9a44d7af784558
iKanae <applamoffy@163.com>
2016-03-16 21:37:56 +0800

    findKnn()

--- kd-tree.py
+++ kd-tree.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-class KD_node:
+class KD:
     def __init__(self,root=None,left=None,right=None,di=None):
         self.root = root
         self.left = left
@@ -26,7 +26,7 @@ def createKdtree(datalist):
                 di = i
         datalist.sort(key=lambda x: x[di])
         root=datalist[lens/2]
-        tree=KD_node(root=root,di=di)
+        tree=KD(root=root,di=di)
         tree.left=createKdtree(datalist[0:(lens/2)])
         tree.right=createKdtree(datalist[lens/2+1:lens])
         return tree
@@ -39,13 +39,38 @@ def dist(a,b):
 
 def findKnn(goal,tree):
     root=tree.root
-    dis=dist(goal,root)
-    while root:
-        dis_left=dist(goal,tree.left.root)
-        dis_right=dist(goal,tree.right.root)
-    return dis
+    min_dist=dist(goal,root)
+    list=[]
+    temp_tree=tree
+    while temp_tree:
+        list.append(temp_tree)
+        dis=dist(goal,temp_tree.root)
+        if min_dist>dis:
+            root=temp_tree.root
+            min_dist=dis
+        temp_di=temp_tree.di
+        if goal[temp_di]<=temp_tree.root[temp_di]:
+            temp_tree=temp_tree.left
+        else:
+            temp_tree=temp_tree.right
+    while list:
+        back_tree=list.pop()
+        temp_di=back_tree.di
+        if abs(goal[temp_di] - back_tree.root[temp_di]) < min_dist:
+            if goal[temp_di] <= back_tree.root[temp_di]:
+                temp_tree = back_tree.right
+            else:
+                temp_tree = back_tree.left
+
+            if temp_tree:
+                list.append(temp_tree)
+                temp_dist = dist(goal, temp_tree.root)
+                if min_dist > temp_dist:
+                    min_dist = temp_dist
+                    root = temp_tree.root
+    return root, min_dist
 
 
-#data=[[2,3],[5,4],[9,6],[4,7],[8,1],[7,2]]
-#data_tree=createKdtree(data)
-#data2=[[2,3]]
+data=[[2,3],[5,4],[9,6],[4,7],[8,1],[7,2]]
+data_tree=createKdtree(data)
+print findKnn([1,2],data_tree)
